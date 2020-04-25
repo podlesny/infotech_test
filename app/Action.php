3fa809c566e9cc370968334b86b36bbda70d9d7c
Max Podlesny <mpodliesny@gmail.com>
2020-04-25 14:07:10 +0300

    Added relationships for models

--- app/Action.php
+++ app/Action.php
@@ -6,8 +6,33 @@ use Illuminate\Database\Eloquent\Model as Eloquent;
 
 class Action extends Eloquent{
 
-	protected $guarded = ['id'];
+    protected $guarded = ['id'];
 
-	public $timestamps = false;
+    public $timestamps = false;
+
+    public function nextStep(){
+        return $this->belongsTo(Step::class, 'next_id', 'id');
+    }
+
+    public function requiredObjects(){
+        return $this->hasMany(RequiredObject::class);
+    }
+
+    public function getRequiredObjects(){
+        $objects = $this->requiredObjects;
+        $arr = [];
+        foreach($objects as $obj){
+            if($obj->random){
+                $rand = mt_rand(1,100);
+                if($rand > 50){
+                    $arr[] = $obj;
+                }
+            }
+            else{
+                $arr[] = $obj;
+            }
+        }
+        return $arr;
+    }
 
 }
